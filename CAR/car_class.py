e0e9bed55ba6098b0b040eb2144fcecec6b414cd
gerikah <aldaygerikah@gmail.com>
2023-07-05 03:24:47 +0800

    def car attributes

--- CAR/car_class.py
+++ CAR/car_class.py
@@ -1,10 +1,15 @@
 # Write a class named Car that has the following data attributes:
 class Car:
-    
-# _ _year_model (for the car’s year model)
-# _ _make (for the make of the car)
-# _ _speed (for the car’s current speed)
 
+    def __init__(self, year_model, make):
+
+        # _ _year_model (for the car’s year model)
+        self.__year_model = year_model
+        # _ _make (for the make of the car)
+        self.__make = make
+        # _ _speed (for the car’s current speed)
+        self.__speed = 0
+        
 # The Car class should have an _ _init_ _ method that accepts the car’s year model and make as arguments. These values should be assigned to the object’s _ _year_model and _ _make data attributes. It should also assign 0 to the _ _speed data attribute.
 
 # The class should also have the following methods:
