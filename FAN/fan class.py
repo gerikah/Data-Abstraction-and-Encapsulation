c1e4a292d78846f2dcd9d29dbfd564f8794a259c
gerikah <aldaygerikah@gmail.com>
2023-07-05 02:25:58 +0800

    attributes of fan

--- FAN/fan class.py
+++ FAN/fan class.py
@@ -6,10 +6,16 @@ class Fan:
     MEDIUM = 2
     FAST = 3
 
-# A private int data field named speed that specifies the speed of the fan.
-# A private bool data field named on that specifies whether the fan is on (the default is False).
-# A private float data field named radius that specifies the radius of the fan.
-# A private string data field named color that specifies the color of the fan.
+    def __init__(self, speed = SLOW, radius = 5, color = "Blue", on = False):
+        # A private int data field named speed that specifies the speed of the fan.
+        self.__fan_speed = speed
+        # A private bool data field named on that specifies whether the fan is on (the default is False).
+        self.__fan_status = on
+        # A private float data field named radius that specifies the radius of the fan.
+        self.__fan_radius = radius
+        # A private string data field named color that specifies the color of the fan.
+        self.__fan_color = color
+
 # The accessor(getters)  and mutator(setters)  methods for all four data fields.
 # A constructor that creates a fan with the specified speed (default SLOW), radius (default 5), color (default blue), and on (default False).
 # Write a test program named TestFan that creates two Fan objects. For the first object, assign the maximum speed, radius 10, color yellow, and turn it on. Assign medium speed, radius 5, color blue, and turn it off for the second object. Display each object’s speed, radius, color, and on properties.
