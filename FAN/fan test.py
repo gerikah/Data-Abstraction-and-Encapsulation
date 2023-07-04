68dff85b3408eff7815e7ce642cc373bd061cd1f
gerikah <aldaygerikah@gmail.com>
2023-07-05 02:40:37 +0800

    TestFan class added

--- FAN/fan test.py
+++ FAN/fan test.py
@@ -1,1 +1,4 @@
 # Write a test program named TestFan that creates two Fan objects. For the first object, assign the maximum speed, radius 10, color yellow, and turn it on. Assign medium speed, radius 5, color blue, and turn it off for the second object. Display each object’s speed, radius, color, and on properties.
+
+# create a test program with class namednTestFan
+class TestFan:
