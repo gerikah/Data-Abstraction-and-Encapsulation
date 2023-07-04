fa7e4c8fcc0b7a3185544ad0996ecf80bed0d285
gerikah <aldaygerikah@gmail.com>
2023-07-05 03:34:39 +0800

    methods done

--- CAR/car_class.py
+++ CAR/car_class.py
@@ -9,15 +9,21 @@ class Car:
         self.__make = make
         # _ _speed (for the car’s current speed)
         self.__speed = 0
-        
-# The Car class should have an _ _init_ _ method that accepts the car’s year model and make as arguments. These values should be assigned to the object’s _ _year_model and _ _make data attributes. It should also assign 0 to the _ _speed data attribute.
 
-# The class should also have the following methods:
-# accelerate()
-# The accelerate method should add 5 to the speed data attribute each time it is called.
-# brake()
-# The brake method should subtract 5 from the speed data attribute each time it is called.
-# get_speed()
-# The get_speed method should return the current speed.
+    # The class should also have the following methods:
+    # accelerate()
+    def accelerate(self):
+        self.__speed += 5
+    # The accelerate method should add 5 to the speed data attribute each time it is called.
+    
+    # brake()
+    def brake(self):
+        self.__speed -= 5
+    # The brake method should subtract 5 from the speed data attribute each time it is called.
+    
+    # get_speed()
+    def get_speed(self):
+        return self.__speed
+    # The get_speed method should return the current speed.
 
 # Next, design a program that creates a Car object then calls the accelerate method five times. After each call to the accelerate method, get the current speed of the car and display it. Then call the brake method five times. After each call to the brake method, get the current speed of the car and display it.
