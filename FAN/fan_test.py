0c09ce319a2011e8d97baeed23b33e78b458fda1
gerikah <aldaygerikah@gmail.com>
2023-07-05 02:48:06 +0800

    2 Fan objects created -- files was renamed

--- FAN/fan_test.py
+++ FAN/fan_test.py
@@ -1,4 +1,8 @@
 # Write a test program named TestFan that creates two Fan objects. For the first object, assign the maximum speed, radius 10, color yellow, and turn it on. Assign medium speed, radius 5, color blue, and turn it off for the second object. Display each object’s speed, radius, color, and on properties.
-
+from fan_class import Fan
 # create a test program with class namednTestFan
 class TestFan:
+    def test(self):
+
+        Fan_1 = Fan(Fan.FAST, 10, "Yellow", True)
+        Fan_2 = Fan(Fan.MEDIUM, 5, "Blue", False)
