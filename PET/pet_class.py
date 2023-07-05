f211535692590673092a6efad7522e3c515096a4
gerikah <aldaygerikah@gmail.com>
2023-07-06 00:16:24 +0800

    class created

--- PET/pet_class.py
+++ PET/pet_class.py
@@ -1,4 +1,6 @@
-# Write a class named Pet, which should have the following data attributes:
+# create a class named Pet 
+class Pet:
+# which should have the following data attributes:
 # _ _name (for the name of a pet)
 # _ _animal_type (for the type of animal that a pet is. Example values are ‘Dog’, ‘Cat’, and ‘Bird’)
 # _ _age (for the pet’s age)
