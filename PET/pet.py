504095fdabe1dd0d51ae00359edf8645c4a0c947
gerikah <aldaygerikah@gmail.com>
2023-07-06 00:38:13 +0800

    create object and setting its attributes

--- PET/pet.py
+++ PET/pet.py
@@ -2,8 +2,15 @@
 from pet_class import Pet
 
 # Once you have written the class, write a program that creates an object of the class and prompts the user to enter the name, type, and age of his or her pet. This data should be stored as the object’s attributes. Use the object’s accessor methods to retrieve the pet’s name, type, and age and display this data on the screen.
+# object of the Pet class
+pet = Pet()
 
 # prompts the user to enter the name, type, and age of his or her pet
 name = input ("Enter the name of your pet: ")
 animal_type = input ("Enter the type of animal your pet is: ")
 age = int(input("Enter the age of your pet: "))
+
+# setting the attributes
+pet.set_name(name)
+pet.set_animal_type(animal_type)
+pet.set_age(age)
