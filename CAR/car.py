f46f6e41b3f7b703d8c17100c5fac2fd508ecb09
gerikah <aldaygerikah@gmail.com>
2023-07-05 23:21:24 +0800

    brake method

--- CAR/car.py
+++ CAR/car.py
@@ -3,11 +3,14 @@ from car_class import Car
 # car object
 car = Car(2022, "Example Make")
 
-# then calls the accelerate method five times. 
+# call the accelerate method five times. 
 print("Accelerating: ")
 for i in range(5):
     car.accelerate()
     print(f"Current Speed: {car.get_speed()}")
 
-# after each call to the accelerate method, get the current speed of the car and display it. 
 # then call the brake method five times. After each call to the brake method, get the current speed of the car and display it.
+print("Braking: ")
+for i in range(5):
+    car.brake()
+    print(f"Current Speed: {car.get_speed()}")
