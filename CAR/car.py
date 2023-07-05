62dbfe03600e4604e08a9af4bf72d84b905c9da3
gerikah <aldaygerikah@gmail.com>
2023-07-05 23:15:53 +0800

    car object created

--- CAR/car.py
+++ CAR/car.py
@@ -1,2 +1,8 @@
 from car_class import Car
-# Next, design a program that creates a Car object then calls the accelerate method five times. After each call to the accelerate method, get the current speed of the car and display it. Then call the brake method five times. After each call to the brake method, get the current speed of the car and display it.
+
+# car object
+car = Car(2022, "Example Make")
+
+# then calls the accelerate method five times. 
+# after each call to the accelerate method, get the current speed of the car and display it. 
+# then call the brake method five times. After each call to the brake method, get the current speed of the car and display it.
