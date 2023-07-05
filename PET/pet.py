48d73d154c0e656924d8d6960d3e97031e55873e
gerikah <aldaygerikah@gmail.com>
2023-07-06 00:41:20 +0800

    display infos

--- PET/pet.py
+++ PET/pet.py
@@ -1,7 +1,6 @@
 # import Pet from pet_class.py
 from pet_class import Pet
 
-# Once you have written the class, write a program that creates an object of the class and prompts the user to enter the name, type, and age of his or her pet. This data should be stored as the object’s attributes. Use the object’s accessor methods to retrieve the pet’s name, type, and age and display this data on the screen.
 # object of the Pet class
 pet = Pet()
 
@@ -14,3 +13,9 @@ age = int(input("Enter the age of your pet: "))
 pet.set_name(name)
 pet.set_animal_type(animal_type)
 pet.set_age(age)
+
+# display the informations
+print("Pet Details")
+print("Pet's Name:", pet.get_name)
+print("Animal Type:", pet.get_animal_type)
+print("Age:", pet.get_age)
