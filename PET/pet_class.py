6166a5f0683d4f94d5990136d5b9c0a108a6dbfe
gerikah <aldaygerikah@gmail.com>
2023-07-06 00:24:39 +0800

    name field set and get

--- PET/pet_class.py
+++ PET/pet_class.py
@@ -6,19 +6,21 @@ class Pet:
         self.__animal_type = " "
         self__age = 0
 
-    # assign value to __name field
-    def set_name(self, name):
+    # set and get for __name field
+    def set_name (self, name):
         self.__name = name
-
-# set_animal_type()
-# This method assigns a value to the _ _animal_type field.
-# set_age()
-# This method assigns a value to the _ _age field.
-# get_name()
-# This method returns the value of the _ _ name field.
-# get_animal_type()
-# This method returns the value of the _ _animal_type field.
-# get_age()
-# This method returns the value of the _ _age field.
+    def get_name (self):
+        return self.__name
+    
+    # set_animal_type()
+    # This method assigns a value to the _ _animal_type field.
+    # set_age()
+    # This method assigns a value to the _ _age field.
+    # get_name()
+    # This method returns the value of the _ _ name field.
+    # get_animal_type()
+    # This method returns the value of the _ _animal_type field.
+    # get_age()
+    # This method returns the value of the _ _age field.
 
 # Once you have written the class, write a program that creates an object of the class and prompts the user to enter the name, type, and age of his or her pet. This data should be stored as the object’s attributes. Use the object’s accessor methods to retrieve the pet’s name, type, and age and display this data on the screen.
